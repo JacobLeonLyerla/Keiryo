b6382a22282b4de147eeadb540332796647e27e2
Jacob Leon Lyerla <umasour@yahoo.com>
2021-08-27 18:47:52 -0700

    rendering test graph in app.js

---
+++ keiryo/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import BarGraph from "./testgraph"
+import VisTestGraph from "./visgraphtest";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <VisTestGraph/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
