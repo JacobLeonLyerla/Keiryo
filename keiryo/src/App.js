be6a05914235be065292376cca00674791584dcb
Jacob Leon Lyerla <umasour@yahoo.com>
2021-08-27 19:22:52 -0700

    passing data from app to graph

--- keiryo/src/App.js
+++ keiryo/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import BarGraph from "./testgraph"
 import VisTestGraph from "./visgraphtest";
 
+let data=[
+  { quarter: 1, earnings: 113000, label: "quarter 1" },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+]
+
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
-       <VisTestGraph/>
+       <VisTestGraph data={data}/>
       
       </header>
     </div>
