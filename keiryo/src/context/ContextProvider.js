16a4a1b900b4c3755b962369c0ced0c3d60f36a4
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-07 16:41:03 -0700

    Created context provider boiler plate code

---
+++ keiryo/src/context/ContextProvider.js
@@ -0,0 +1,12 @@
+import Reactk, { useState } from "react";
+import AppContext from ".";
+
+const ContextProvider = ({ children }) => {
+  const [example, setExample] = useState("Hello there");
+  const context = {
+    setExample,
+    example,
+  };
+
+  return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
+};
