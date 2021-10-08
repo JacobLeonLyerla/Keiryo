0461ca1c2bed8f3d7c8d9815db73ff0f07c4045b
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-07 17:29:39 -0700

    added in context to app.js everything is working correctly

--- keiryo/src/App.js
+++ keiryo/src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
-import './App.css';
-import BarGraph from "./testgraph"
+import React, { useContext, useEffect, useState } from "react";
+import "./App.css";
+import AppContext from "./context";
+import BarGraph from "./testgraph";
 import VisTestGraph from "./visgraphtest";
 
-let data=[
-  { quarter: 1, earnings: 113000, label: "quarter 1" },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 }
-]
+// let data=[
+//   { quarter: 1, earnings: 113000, label: "quarter 1" },
+//   { quarter: 2, earnings: 16500 },
+//   { quarter: 3, earnings: 14250 },
+//   { quarter: 4, earnings: 19000 }
+// ]
 
-function App() {
- 
+const App = () => {
+  const { example, setExample } = useContext(AppContext);
+  const [formText, setFormText] = useState("");
+
+  useEffect(() => {
+    console.log("Context Here: ", example);
+  }, [example]);
+
+  const handleChange = (e) => {
+    setFormText([e.target.date, e.target.value]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setExample(formText);
+  };
   return (
     <div className="App">
       <header className="App-header">
-       <VisTestGraph data={data}/>
-      
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="example">Example:</label>
+          <input type="text" date={date} value={formText} onChange={handleChange} />
+          <button>Submit</button>
+        </form>
+
+        {/* <VisTestGraph data={data}/> */}
       </header>
     </div>
   );
-}
+};
 
 export default App;
