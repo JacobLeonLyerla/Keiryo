dbfbb8fb1843070aef9825cf9ce33427d1b46cec
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-08 19:08:06 -0700

    passed data from context to graph. Hard coded date at this point

--- keiryo/src/context/ContextProvider.js
+++ keiryo/src/context/ContextProvider.js
@@ -4,7 +4,10 @@ import AppContext from ".";
 const ContextProvider = ({ children }) => {
   
 
-  const [example, setExample] = useState({date:null,weight:null});
+  const [example, setExample] = useState([{date: 1, body: 113000},
+    {date: 2, body: 16500},
+    {date: 3, body: 14250},
+    {date: 4, body: 19000,}],);
   
   const context = {
     setExample,
